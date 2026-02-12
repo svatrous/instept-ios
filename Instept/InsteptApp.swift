eabfd3ccad4934cd9781946bbe63b568c17fe999
Pavel Ilin <svatorus@gmail.com>
2026-02-12 16:44:45 +0400

    Initial Commit

---
+++ Instept/InsteptApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsteptApp.swift
+//  Instept
+//
+//  Created by Pavel Ilin on 12.02.26.
+//
+
+import SwiftUI
+
+@main
+struct InsteptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InsteptTests/InsteptTests.swift
@@ -0,0 +1,16 @@
+//
+//  InsteptTests.swift
+//  InsteptTests
+//
+//  Created by Pavel Ilin on 12.02.26.
+//
+
+import Testing
+
+struct InsteptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
